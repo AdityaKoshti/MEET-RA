d92841f3c37f2c5f67d948c10ce94301e02522ab
AdityaKoshti <adityadk1920@gmail.com>
2022-12-30 13:33:09 +0530

    added start

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({
 
 const { getAuth, createUserWithEmailAndPassword } = require('firebase/auth')
 const admin = require('firebase-admin')
-const credentials = require('./src/serviceAccountKey.json');
+// const credentials = require('./src/serviceAccountKey.json');
 const { get } = require('https');
 
 
